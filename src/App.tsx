4fd54c07fd5b9f0dd53f57dd5301f1e302d5c3dd
fh <fhlqwe@gmail.com>
2024-05-22 10:53:14 +0800

    chore: remove unused dependency and commented code

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
+// import { Button,DatePicker } from 'antd';
 import dayjs from 'dayjs';
-import { ProTable } from '@ant-design/pro-components';
 /**
- * 如果注释ProTable 值为 2024-02-01 00:00:00
- * 如果不注释ProTable 值为 2024-05-20 00:00:00 ，日期为当天
+ * 如果注释DatePicker/ Button 值为 2024-02-01 00:00:00
+ * 如果不注释DatePicker/ Button 值为 2024-05-20 00:00:00 ，日期为当天
  */
 function App() {
   return (
     <>
     {dayjs("2024-02","gggg-ww").format("YYYY-MM-DD HH:mm:ss")}
-    <ProTable/>
+    {/* <DatePicker/> */}
+    {/* <Button>222</Button> */}
     </>
   )
 }
-
 export default App
