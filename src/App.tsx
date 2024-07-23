dbe5b764c66bd6e9be81e2f5d78e73b0684d44ba
fh <fhlqwe@gmail.com>
2024-07-23 15:13:38 +0800

    chore: Update npm dependencies and remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,25 @@
-import type { FormProps } from "antd";
-import { Button, DatePicker, Form } from "antd";
-import type React from "react";
-import "./index.css";
+import { DatePicker } from 'antd'
+import dayjs from 'dayjs'
+import type React from 'react'
+import dayjsLocal from 'dayjs/locale/pt-br'
 
-const onFinish: FormProps["onFinish"] = (values) => {
-  console.log("Success:", values);
-};
-
-const onFinishFailed: FormProps["onFinishFailed"] = (errorInfo) => {
-  console.log("Failed:", errorInfo);
-};
+dayjs.locale(dayjsLocal)
 
 const App: React.FC = () => (
-  <Form
-    name="basic"
-    labelCol={{ span: 8 }}
-    wrapperCol={{ span: 16 }}
-    style={{ maxWidth: 600 }}
-    initialValues={{ remember: true }}
-    onFinish={onFinish}
-    onFinishFailed={onFinishFailed}
-    autoComplete="off"
-  >
-    <Form.Item label="date" name="date">
-      <DatePicker />
-    </Form.Item>
+  <div>
+    <DatePicker
+      onChange={(value) => {
+        console.info('date change', value.toISOString())
+      }}
+    />
 
-    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-      <Button type="primary" htmlType="submit">
-        Submit
-      </Button>
-    </Form.Item>
-  </Form>
-);
+    <DatePicker
+      picker="week"
+      onChange={(value) => {
+        console.info('date change', value.toISOString())
+      }}
+    />
+  </div>
+)
 
-export default App;
+export default App
